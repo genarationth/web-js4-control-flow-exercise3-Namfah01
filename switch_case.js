b6dd49bae38d1701649186074a189900cddca178
Namfah01 <kanokwanbing09@gmail.com>
2024-05-14 11:04:27 +0700

    ex3

---
+++ switch_case.js
@@ -0,0 +1,17 @@
+
+
+const dayNumber = 1;
+
+if (dayNumber === 0 || dayNumber === 6) {
+    console.log("It is the weekend!");
+} else if (dayNumber >= 1 && dayNumber <= 5) {
+    console.log("It is the weekday!");
+} else {
+    console.log("Invalid day number");
+}
+
+
+
+
+
+
